558830166bcd44833c8949762f63f05f031c6101
JACK <jack99541008@gmail.com>
2015-12-25 21:07:13 +0800

    final

---
+++ test/test.js
@@ -0,0 +1,9 @@
+describe('Test > ', function() {
+  beforeEach(function() {
+  });
+
+  it('will test some pure functions', function() {
+    // Write any pure function assertion here.
+    assert.equal(1+2, 3);
+  });
+});
